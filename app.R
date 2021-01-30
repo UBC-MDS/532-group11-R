cd18b7c757cdac979dcaa6432a32b08df8f0a004
yaz-saleh <salehyaz@student.ubc.ca>
2021-01-30 01:02:41 -0500

    Add profit vs genre plot

--- app.R
+++ app.R
@@ -2,10 +2,34 @@ library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
 library(dashBootstrapComponents)
+library(tidyverse)
+library(here)
+library(plotly)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
+### Data loading and cleaning
 
+set_here('/home/yazan/ubc-mds/block_4/532/532-group11-R')
+data_path <- paste0(here(), '/data/processed/processed_movie_data.csv')
+data <-  read_csv(data_path)
+
+data$release_date <- as.Date(data$release_date)
+data$release_month <- month(data$release_month, label = TRUE)
+
+
+profit_genre_data <- data %>% 
+  group_by(release_month, genres) %>% 
+  summarise(med_profit = median(profit))
+
+proft_genre_plot <- ggplot(data = profit_genre_data) +
+  aes(x = release_month,
+      y = med_profit,
+      group = genres,
+      color = genres) +
+  geom_line() +
+  scale_y_continuous(labels = scales::dollar) +
+  labs(x = "Release Month", y = "Median Profit", color = "Genres")
 
 app$layout(dbcContainer(list(
   htmlH1("Movie Production Planner"),
@@ -63,22 +87,21 @@ app$layout(dbcContainer(list(
           )
         ))
       )),
-    dbcRow(list(dbcCol(
-      list(htmlBr(),
-           htmlLabel(
-             "Find some potential actors",
-             style = list("font-size" = 20)
-           ))
-    ),
-    dbcCol(
-      list(htmlBr(),
-           htmlLabel(
-             "Plan your release month",
-             style = list("font-size" = 20)
-           ))
-    )))
+      dbcRow(list(
+        dbcCol(list(
+          htmlBr(),
+          htmlLabel("Find some potential actors",
+                    style = list("font-size" = 20))
+        )),
+        dbcCol(list(
+          htmlBr(),
+          htmlLabel("Plan your release month",
+                    style = list("font-size" = 20)),
+          dccGraph(figure=ggplotly(proft_genre_plot))
+        ))
+      )))
   )))
-))))
+)))
 
 
 
